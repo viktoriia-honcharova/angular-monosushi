7d31577d54c3271f28f4dedf66a2a112f4c0bd1d
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-04-05 12:41:26 +0300

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { DiscountComponent } from './pages/discount/discount.component';
+import { DiscountInfoComponent } from './pages/discount-info/discount-info.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductInfoComponent } from './pages/product-info/product-info.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'actions', component: DiscountComponent },
+  { path: 'product-category/:category', component: ProductComponent },
+  { path: 'dostavka-ta-oplata', component: DeliveryComponent },
+  { path: 'about-us', component: AboutComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
