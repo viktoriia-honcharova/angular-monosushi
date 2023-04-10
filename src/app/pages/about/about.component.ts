c3525c8a59e13f5fb8397e7df32dbc7402c5c6c9
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-04-10 09:44:00 +0300

    Finished the about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
+  q1 = false;
+  q2 = false;
+  q3 = false;
+  q4 = false;
 
+  displayQ1(): void {
+    this.q1 = !this.q1;
+    this.q2 = false;
+    this.q3 = false;
+    this.q4 = false;
+  }
+
+  displayQ2(): void {
+    this.q2 = !this.q2;
+    this.q1 = false;
+    this.q3 = false;
+    this.q4 = false;
+  }
+
+  displayQ3(): void {
+    this.q3 = !this.q3;
+    this.q1 = false;
+    this.q2 = false;
+    this.q4 = false;
+  }
+
+  displayQ4(): void {
+    this.q4 = !this.q4;
+    this.q1 = false;
+    this.q2 = false;
+    this.q3 = false;
+  }
 }
