5542df91bdd518a0e817d271f6b8252ccc842266
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-04-10 21:40:39 +0300

    Added admin components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,11 @@ import { DeliveryComponent } from './pages/delivery/delivery.component';
 import { AboutComponent } from './pages/about/about.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { OfertaComponent } from './pages/oferta/oferta.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoryComponent } from './admin/admin-category/admin-category.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminDiscountComponent } from './admin/admin-discount/admin-discount.component';
+import { AdminOrderComponent } from './admin/admin-order/admin-order.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +35,11 @@ import { OfertaComponent } from './pages/oferta/oferta.component';
     AboutComponent,
     CheckoutComponent,
     OfertaComponent,
+    AdminComponent,
+    AdminCategoryComponent,
+    AdminProductComponent,
+    AdminDiscountComponent,
+    AdminOrderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
