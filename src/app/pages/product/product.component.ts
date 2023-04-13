5fdbc4ecb01283be84d8fbc7f0d31c1a16c12f4c
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-04-13 17:13:39 +0300

    Finished the product component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProductResponse } from 'src/app/shared/interfaces/product.interface';
+import { ProductService } from 'src/app/shared/services/product/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  public userProducts!: IProductResponse[];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getAll().subscribe((data) => {
+      this.userProducts = data;
+    });
+  }
 }
